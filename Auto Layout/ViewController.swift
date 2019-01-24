24c80ff523a796a28473b7156457d28c5d802354
Giuseppe Sapienza <sapienza.giuseppe93@gmail.com>
2019-01-24 11:26:40 +0100

    Initial Commit

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Giuseppe Sapienza on 14/01/19.
+//  Copyright © 2019 Giuseppe Sapienza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
